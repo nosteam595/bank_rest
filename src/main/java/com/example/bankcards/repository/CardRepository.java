6e2af894a20af4cf16ec43bbb5c0ed397d1353d9
nosteam595 <nosteam@bk.ru>
2026-05-19 14:15:55 +0300

    Создание repository User и Card

---
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -0,0 +1,17 @@
+package com.example.bankcards.repository;
+
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card,Long> {
+    Page<Card> findByUser(User user, Pageable pageable);
+    Optional<Card> findByIdAndUser(Long id, User user);
+    Optional<Card> findByCardNumberEncrypted(String cardNumberEncrypted);
+}
