658583d07c86acde2d26a5052759affb476f97c5
nosteam595 <nosteam@bk.ru>
2026-05-20 17:01:45 +0300

    Создание слоя контроллеров

---
+++ src/main/java/com/example/bankcards/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.example.bankcards.controller;
+
+import com.example.bankcards.dto.AuthRequest;
+import com.example.bankcards.dto.AuthResponse;
+import com.example.bankcards.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
---
+++ src/main/java/com/example/bankcards/controller/CardController.java
@@ -0,0 +1,64 @@
+package com.example.bankcards.controller;
+
+import com.example.bankcards.dto.CardCreateRequest;
+import com.example.bankcards.dto.CardResponse;
+import com.example.bankcards.dto.CardStatusUpdateRequest;
+import com.example.bankcards.dto.TransferRequest;
+import com.example.bankcards.service.CardService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/cards")
+public class CardController {
+
+    private final CardService cardService;
+
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CardResponse> createCard(@Valid @RequestBody CardCreateRequest request) {
+        return new ResponseEntity<>(cardService.createCard(request), HttpStatus.CREATED);
+    }
+
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<CardResponse> updateCardStatus(
+            @PathVariable Long id,
+            @Valid @RequestBody CardStatusUpdateRequest request) {
+        return ResponseEntity.ok(cardService.updateCardStatus(id, request));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<CardResponse>> getAllCards(Pageable pageable) {
+        return ResponseEntity.ok(cardService.getAllCards(pageable));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCard(@PathVariable Long id) {
+        cardService.deleteCard(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping("/my")
+    public ResponseEntity<Page<CardResponse>> getMyCards(Pageable pageable) {
+        return ResponseEntity.ok(cardService.getMyCards(pageable));
+    }
+
+    @PostMapping("/my/block/{id}")
+    public ResponseEntity<CardResponse> userBlockCard(@PathVariable Long id) {
+        return ResponseEntity.ok(cardService.requestBlockCard(id));
+    }
+
+    @PostMapping("/my/transfer")
+    public ResponseEntity<String> transferBetweenOwnCards(@Valid @RequestBody TransferRequest request) {
+        cardService.transferBetweenOwnCards(request);
+        return ResponseEntity.ok("Перевод успешно выполнен");
+    }
+}
